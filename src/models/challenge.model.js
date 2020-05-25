a95b91042bca54010314e7fc3222bb7981b6f2dd
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-05-25 18:33:47 -0400

    Add canSupportNumPlayers method

--- src/models/challenge.model.js
+++ src/models/challenge.model.js
@@ -1,23 +1,18 @@
-const challenges = [
-    new Challenge(
-        "Odd Man Out",
-        "P"
-    ),
-    new Challenge(
-        "Odd Man Out",
-        "",
-    ),
-];
+const challenges = [ new Challenge('Odd Man Out', 'P'), new Challenge('Odd Man Out', '') ];
 
 export class Challenge {
-    static challenges = challenges;
+	static challenges = challenges;
 
-    constructor(title, description, maxPlayers, minPlayers, questions) {
-        this.title = title;
-        this.description = description;
-        this.rules = rules;
-        this.maxPlayers = maxPlayers;
-        this.minPlayers = minPlayers;
-        this.questions = questions;
-    }
+	constructor(title, description, maxPlayers, minPlayers, questions) {
+		this.title = title;
+		this.description = description;
+		this.rules = rules;
+		this.maxPlayers = maxPlayers;
+		this.minPlayers = minPlayers;
+		this.questions = questions;
+	}
+
+	canSupportNumPlayers(numPlayers) {
+		return numPlayers >= this.minPlayers && numPlayers <= this.maxPlayers;
+	}
 }
