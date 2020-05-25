a693e9f5a18f22be5597149469837da329fffa56
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-05-25 18:34:12 -0400

    Remove quiz

--- src/models/episode.model.js
+++ src/models/episode.model.js
@@ -1,14 +1,11 @@
 import { Challenge } from './challenge.model';
-import { Quiz } from './quiz.model';
 
 export class Episode {
-    challenges = [];
-    quiz = null;
-    numPlayers = -1;
+	challenges = [];
+	numPlayers = -1;
 
-    constructor(numPlayers, challenges, quiz) {
-        this.challenges = challenges;
-        this.quiz = quiz;
-        this.numPlayers = numPlayers;
-    }
+	constructor(numPlayers, challenges) {
+		this.challenges = challenges;
+		this.numPlayers = numPlayers;
+	}
 }
