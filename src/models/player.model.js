6afe0981cf91c6e2f5bfaeb221789181b7a46115
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-05-30 12:47:36 -0400

    Fix player setting issue

--- src/models/player.model.js
+++ src/models/player.model.js
@@ -1,34 +1,30 @@
 class Player {
-    constructor(name) {
-        this.name = name;
-    }
+	constructor(name) {
+		this.name = name;
+	}
 }
 
 export class PlayerCreator {
-    constructor() {}
+	constructor() {}
 
-    static createPlayer(player) {
-        if (!player) {
-            return {
-                success: false,
-                errors: [
-                    { error: 'player_data_not_given' }
-                ]
-            };
-        }
+	static createPlayer(player) {
+		if (!player) {
+			return {
+				success: false,
+				errors: [ { error: 'player_data_not_given' } ]
+			};
+		}
 
-        if (!player.name) {
-            return {
-                success: false,
-                errors: [
-                    { error: 'name_not_given' }
-                ]
-            };
-        }
+		if (!player.name) {
+			return {
+				success: false,
+				errors: [ { error: 'name_not_given' } ]
+			};
+		}
 
-        return {
-            success: true,
-            player: new Player(player)
-        };
-    }
+		return {
+			success: true,
+			player: new Player(player.name)
+		};
+	}
 }
