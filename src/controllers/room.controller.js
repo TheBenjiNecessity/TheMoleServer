13666d6425f01cc1abe4e1baff6663ed95296474
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-05-30 12:46:59 -0400

    Remove numbers

--- src/controllers/room.controller.js
+++ src/controllers/room.controller.js
@@ -31,18 +31,7 @@ class RoomController {
 			'W',
 			'X',
 			'Y',
-			'Z',
-			'0',
-			'1',
-			'2',
-			'3',
-			'4',
-			'5',
-			'6',
-			'7',
-			'8',
-			'9',
-			'0'
+			'Z'
 		];
 
 		this.episodeService = new EpisodeService();
