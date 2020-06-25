4a1b017f376240539d7b3d0979e45d224690c780
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-06-24 21:05:53 -0400

    Add new models

---
+++ src/models/challenges/challenge.model.js
@@ -0,0 +1,37 @@
+import RaisedHand from '../raisedHand.model';
+
+export default class Challenge {
+	constructor(title, type, description, maxPlayers, minPlayers, questions) {
+		this.title = title;
+		this.type = type;
+		this.description = description;
+		this.maxPlayers = maxPlayers;
+		this.minPlayers = minPlayers;
+		this.questions = questions;
+		this.state = 'roles';
+		this.agreedPlayers = [];
+		this.raisedHands = [];
+	}
+
+	canSupportNumPlayers(numPlayers) {
+		return numPlayers >= this.minPlayers && numPlayers <= this.maxPlayers;
+	}
+
+	addAgreedPlayer(player) {
+		let foundPlayer = this.agreedPlayers.find((p) => p.name === player.name);
+		if (!foundPlayer) {
+			this.agreedPlayers.push(player);
+		}
+	}
+
+	raiseHandForPlayer(player, role) {
+		let foundRaisedHand = this.raisedHands.find((r) => r.player.name === player.name);
+
+		if (foundRaisedHand) {
+			let indexOfRaisedHand = this.raisedHands.indexOf(foundRaisedHand);
+			this.raisedHands[indexOfRaisedHand].role = role;
+		} else {
+			this.raisedHands.push(new RaisedHand(player, role));
+		}
+	}
+}
---
+++ src/models/raisedHand.model.js
@@ -0,0 +1,6 @@
+export default class RaisedHand {
+	constructor(player, role) {
+		this.player = player;
+		this.role = role;
+	}
+}
