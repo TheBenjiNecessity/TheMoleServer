cf7add24eb76a628580efb46ab2fbd0f739fcc43
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-07-18 14:39:09 -0400

    Add methods

--- src/controllers/challenge-controllers/platter-challenge.controller.js
+++ src/controllers/challenge-controllers/platter-challenge.controller.js
@@ -1,17 +1,23 @@
 import ChallengeController from '../challenge.controller';
+import RoomHandlerCreator from '../room.controller';
+import WebSocketServiceCreator from '../../services/websocket.service';
 
-export class PlatterChallengeController extends ChallengeController {
-    constructor(webSocketController) {
-        super(webSocketController);
-        
+export default class PlatterChallengeController extends ChallengeController {
+	constructor() {}
 
-    }
+	chooseExemption({ room, player }) {
+		RoomHandlerCreator.getInstance().giveObjectsToPlayer(room.roomcode, player, 'exemption', 1);
+		let newRoom = RoomHandlerCreator.getInstance().rooms[room.roomcode];
+		WebSocketServiceCreator.getInstance().sendToRoom(room.roomcode, 'took-exemption', newRoom);
+	}
 
- 
+	chooseMoney({ room, player }) {
+		let newRoom = RoomHandlerCreator.getInstance().rooms[room.roomcode];
+		WebSocketServiceCreator.getInstance().sendToRoom(room.roomcode, 'took-money', newRoom);
+	}
+
+	setupSocket(socket) {
+		socket.on('platter-choose-exemption', this.chooseExemption);
+		socket.on('platter-choose-money', this.chooseMoney);
+	}
 }
-
-/**
- * Challenge:
- *  - skip passed raise hand section
- *  - send message to room to show 
- */
