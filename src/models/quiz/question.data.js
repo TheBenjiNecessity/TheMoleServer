f427e431322be8eb67c760161736862b64858c44
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-07-30 22:35:32 -0400

    Use question data

---
+++ src/models/quiz/question.data.js
@@ -0,0 +1,10 @@
+export default [
+	{
+		text: 'What is the eye color of the mole?',
+		choices: [ 'Blue', 'Green', 'Hazel', 'Gray', 'Multi-colored', 'Brown', 'Amber', 'Other' ]
+	},
+	{
+		text: 'How tall is the mole?',
+		choices: [ 'Blue', 'Green', 'Hazel', 'Gray', 'Multi-colored', 'Brown', 'Amber', 'Other' ]
+	}
+];
