7be4b5e40ee6445b1ad379fc9647092b9f83746f
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-08-06 21:07:50 -0400

    Return challenge data

--- src/services/game/challenge.service.js
+++ src/services/game/challenge.service.js
@@ -1,28 +1,16 @@
 import ArrayUtilsService from '../utils/array-utils.service';
-
-import OutAndSafeChallenge from '../../models/challenges/out-and-safe.challenge';
-import PlatterChallenge from '../../models/challenges/platter.challenge';
-import MoleTalkChallenge from '../../models/challenges/moletalk.challenge';
-import PushYourLuckChallenge from '../../models/challenges/push-your-luck.challenge';
-import StacksChallenge from '../../models/challenges/stacks.challenge';
-import TradersChallenge from '../../models/challenges/traders.challenge';
-import ButtonChallenge from '../../models/challenges/button.challenge';
-
-const challenges = [
-	new OutAndSafeChallenge(),
-	new PlatterChallenge(),
-	new MoleTalkChallenge(),
-	new PushYourLuckChallenge(),
-	new StacksChallenge(),
-	new TradersChallenge(),
-	new ButtonChallenge()
-];
+import challengeData from '../../models/challenges/challenge.data';
 
 export default class ChallengeService {
 	constructor() {}
 
 	static getRandomChallengeForPlayers(numPlayers, currentChallenges) {
-		let numRestrictedChallenges = challenges.filter((c) => c.canSupportNumPlayers(numPlayers));
+		let numRestrictedChallenges = [];
+		for (let challenge of challengeData) {
+			if (challenge.maxPlayers >= numPlayers && challenge.minPlayers <= numPlayers) {
+				numRestrictedChallenges.push(challenge);
+			}
+		}
 
 		if (numRestrictedChallenges.length > 0) {
 			return ArrayUtilsService.getRandomElementNotInOtherArray(numRestrictedChallenges, currentChallenges);
