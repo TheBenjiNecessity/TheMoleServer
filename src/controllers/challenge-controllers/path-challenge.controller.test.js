c6df465a12e7b6ac66a0e0770f078e313a615736
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-08-06 21:08:51 -0400

    Add tests

--- src/controllers/challenge-controllers/path-challenge.controller.test.js
+++ src/controllers/challenge-controllers/path-challenge.controller.test.js
@@ -1,14 +1,37 @@
 import PathChallengeControllerCreator from './path-challenge.controller';
 import Room from '../../models/room.model';
+import RoomControllerCreator from '../room.controller';
 
-test('Checks update room', () => {
+test('Checks "chooseChest" method', () => {
+	let roomcode = 'TEST';
 	let room = Room.getTestRoomWithTenPlayers();
 	room.initCurrentChallenge('path');
+	RoomControllerCreator.getInstance().setRoom(room);
 
 	let pathChallengeController = PathChallengeControllerCreator.getInstance();
-	let newRoom = pathChallengeController.updateRoom(room, 'left', 'chest');
-	let pathChallenge = newRoom.currentChallenge;
+	pathChallengeController.chooseChest({ roomcode, choice: 'left' });
+	let pathChallenge = RoomControllerCreator.getInstance().getRoom(roomcode).currentChallenge;
 
-	expect(pathChallenge.leftVotes).toBe(0);
-	expect(pathChallenge.rightVotes).toBe(0);
+	expect(pathChallenge.currentChoice).toBe('left');
+
+	pathChallengeController.chooseChest({ roomcode, choice: 'right' });
+	pathChallenge = RoomControllerCreator.getInstance().getRoom(roomcode).currentChallenge;
+
+	expect(pathChallenge.currentChoice).toBe('right');
+});
+
+test('Checks "addVoteForChest" method', () => {
+	let roomcode = 'TEST';
+	let room = Room.getTestRoomWithTenPlayers();
+	let player = room.players[0];
+	room.initCurrentChallenge('path');
+	RoomControllerCreator.getInstance().setRoom(room);
+
+	let pathChallengeController = PathChallengeControllerCreator.getInstance();
+	let result = pathChallengeController.addVoteForChest({ roomcode, player, choice: 'left' });
+	room = RoomControllerCreator.getInstance().getRoom(roomcode);
+	let pathChallenge = room.currentChallenge;
+	expect(pathChallenge.votes.left.length).toBe(1);
+	expect(pathChallenge.votes.right.length).toBe(0);
+	expect(result.action).toBe('path-vote-chest');
 });
