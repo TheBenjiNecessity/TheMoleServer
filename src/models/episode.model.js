e6d2fa6c541a9a549cb6f0a7afd8c2078cef64a1
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-08-11 21:45:20 -0400

    Add convenience method

--- src/models/episode.model.js
+++ src/models/episode.model.js
@@ -1,6 +1,13 @@
+import ArrayUtilsService from '../services/utils/array-utils.service';
+
 export class Episode {
 	constructor(numPlayers, challenges) {
 		this.challenges = challenges;
 		this.numPlayers = numPlayers;
+	}
+
+	getQuestions() {
+		let questionsArray = this.challenges.map((c) => c.questions);
+		return ArrayUtilsService.array2dTo1d(questionsArray);
 	}
 }
