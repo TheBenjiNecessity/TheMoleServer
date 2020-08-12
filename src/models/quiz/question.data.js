f0c1086ca25e73f510a8e8d3c8ccc50ac904b469
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-08-12 19:38:51 -0400

    Add possible questions

--- src/models/quiz/question.data.js
+++ src/models/quiz/question.data.js
@@ -10,3 +10,7 @@ export default [
 		choices: [ "Less than 5'", "Between 5' and 5'8\"", 'Between 5\'9" and 6\'3"', '6\'4" or above' ]
 	}
 ];
+//Month mole born
+//province mole born in
+// hair color of the mole
+// What decade mole born
