ea7a2a3e78138b50800ce402ce264069efd4fedf
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-08-11 21:46:36 -0400

    Fix question data

--- src/models/quiz/question.data.js
+++ src/models/quiz/question.data.js
@@ -1,10 +1,12 @@
 export default [
 	{
 		text: 'What is the eye color of the mole?',
+		type: 'choices',
 		choices: [ 'Blue', 'Green', 'Hazel', 'Gray', 'Multi-colored', 'Brown', 'Amber', 'Other' ]
 	},
 	{
 		text: 'How tall is the mole?',
-		choices: [ 'Blue', 'Green', 'Hazel', 'Gray', 'Multi-colored', 'Brown', 'Amber', 'Other' ]
+		type: 'choices',
+		choices: [ "Less than 5'", "Between 5' and 5'8\"", 'Between 5\'9" and 6\'3"', '6\'4" or above' ]
 	}
 ];
