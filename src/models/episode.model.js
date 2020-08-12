d59511557794f0dc6eb6795f8a26e5cf26c66c94
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-08-12 19:38:35 -0400

    Add current challenge to episode

--- src/models/episode.model.js
+++ src/models/episode.model.js
@@ -1,7 +1,20 @@
 import ArrayUtilsService from '../services/utils/array-utils.service';
 
 export class Episode {
+	get currentChallenge() {
+		if (this.episodeIsOver) {
+			return null;
+		}
+
+		return this.challenges[this.currentChallengeIndex];
+	}
+
+	get episodeIsOver() {
+		return this.currentChallengeIndex >= this.challenges.length;
+	}
+
 	constructor(numPlayers, challenges) {
+		this.currentChallengeIndex = 0;
 		this.challenges = challenges;
 		this.numPlayers = numPlayers;
 	}
@@ -9,5 +22,9 @@ export class Episode {
 	getQuestions() {
 		let questionsArray = this.challenges.map((c) => c.questions);
 		return ArrayUtilsService.array2dTo1d(questionsArray);
+	}
+
+	increaseChallengeIndex() {
+		this.currentChallengeIndex++;
 	}
 }
