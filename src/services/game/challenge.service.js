e92e3bb3a6c0245cacc8057bfdebdb5d9e4ebb41
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-08-17 19:50:52 -0400

    Remove unused code

--- src/services/game/challenge.service.js
+++ src/services/game/challenge.service.js
@@ -6,45 +6,6 @@ import PlatterChallenge from '../../models/challenges/platter.challenge';
 export default class ChallengeService {
 	constructor() {}
 
-	/**
-	 * Lists all of the challenges that can support the number of players given
-	 * @param {int} numPlayers the desired number of players to play the challenge
-	 * @returns {[object]} the list of challenges
-	 */
-	static getNumPlayersRestrictedChallengeData(numPlayers) {
-		return challengeData.filter((c) => ChallengeService.canSupportNumPlayers(c, numPlayers));
-	}
-
-	/**
-	 * Gets a challenge from the challenge data that isn't already in use
-	 * @param {int} numPlayers the number of players playing in that challenge
-	 * @param {[object]} challengesToExclude the challenges already loaded into the game
-	 */
-	static getRandomChallengeForPlayers(room, numPlayers, challengesToExclude) {
-		let numRestrictedChallengeData = ChallengeService.getNumPlayersRestrictedChallengeData(numPlayers);
-		if (numRestrictedChallengeData.length > 0) {
-			let numRestrictedChallengeTypes = numRestrictedChallengeData.map((c) => c.type);
-
-			let challengeTypesNotUsed = numRestrictedChallengeTypes.filter(
-				(type) => !challengesToExclude.map((e) => (e ? e.type : '')).includes(type)
-			);
-			let randomTypeNotUsed = ArrayUtilsService.getRandomElement(challengeTypesNotUsed);
-			return ChallengeService.getChallengeForType(room, randomTypeNotUsed);
-		}
-
-		return null;
-	}
-
-	/**
-	 * Checks to see if the challenge given can support the number of players given
-	 * @param {object} challenge the challenge data containing min/max players
-	 * @param {int} numPlayers the number of players playing
-	 * @returns {boolean} whether or not the challenge given supports the number of players
-	 */
-	static canSupportNumPlayers(challenge, numPlayers) {
-		return challenge.maxPlayers >= numPlayers && challenge.minPlayers <= numPlayers;
-	}
-
 	static getChallengeForType(type, room) {
 		switch (type) {
 			case 'path':
