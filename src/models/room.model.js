b96d2266d06833274e2afe2d06c4cb53d8ce70c2
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-08-18 22:48:20 -0400

    Add more possible states

--- src/models/room.model.js
+++ src/models/room.model.js
@@ -9,7 +9,14 @@ export const ROOM_MAX_PLAYERS = 10;
 export const ROOM_STATE = {
 	LOBBY: 'lobby',
 	WELCOME: 'game-welcome',
-	EPISODESTART: 'episode-start'
+	EPISODE_START: 'episode-start',
+	CHALLENGE_INTERMISSION: 'challenge-intermission',
+	IN_CHALLENGE: 'in-episode',
+	PRE_QUIZ_INTERMISSION: 'pre-quiz-intermission',
+	IN_QUIZ: 'in-episode',
+	POST_QUIZ_INTERMISSION: 'post-quiz-intermission',
+	EXECUTION: 'execution',
+	EXECUTION_WRAPUP: 'execution-wrapup'
 };
 
 export default class Room {
