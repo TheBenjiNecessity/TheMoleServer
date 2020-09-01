8eb2d7be8cdb325c53f4496f089db7296a4a5e61
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-08-31 20:45:28 -0400

    Add more questions

--- src/models/quiz/question.data.js
+++ src/models/quiz/question.data.js
@@ -8,9 +8,43 @@ export default [
 		text: 'How tall is the mole?',
 		type: 'choices',
 		choices: [ "Less than 5'", "Between 5' and 5'8\"", 'Between 5\'9" and 6\'3"', '6\'4" or above' ]
+	},
+	{
+		text: 'What month was the mole born in?',
+		type: 'choices',
+		choices: [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		]
+	},
+	{
+		text: 'What decade was the mole born in?',
+		type: 'choices',
+		choices: [ '1940s', '1950s', '1960s', '1970s', '1980s', '1990s', '2000s', '2010s' ]
+	},
+	{
+		text: "What is the mole's hair color?",
+		type: 'choices',
+		choices: [ 'Blonde', 'Brown', 'Black', 'Red', 'Other (dyed)' ]
+	},
+	{
+		text: 'Is the mole male or female?',
+		type: 'choices',
+		choices: [ 'Male', 'Female' ]
+	},
+	{
+		text: 'From what continent was the mole born?',
+		type: 'choices',
+		choices: [ 'North America', 'South America', 'Asia', 'Africa', 'Australia', 'Europe' ]
 	}
 ];
-//Month mole born
-//province mole born in
-// hair color of the mole
-// What decade mole born
