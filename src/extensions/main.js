c5074019f96370ca655b28fface508704329a0d9
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-09-23 00:32:07 -0400

    Add date extensions to main

--- src/extensions/main.js
+++ src/extensions/main.js
@@ -1,9 +1,11 @@
 import arrayExtensions from './array';
 import stringExtensions from './string';
 import numberExtensions from './number';
+import dateExtensions from './date';
 
 export default function initExtensions() {
 	arrayExtensions();
 	stringExtensions();
 	numberExtensions();
+	dateExtensions();
 }
