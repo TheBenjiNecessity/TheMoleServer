6e0d2668b0cff15623640d7e507aec4c7e9d9306
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-09-26 14:25:07 -0400

    Make challenge generic

--- src/tests/episode.sample.js
+++ src/tests/episode.sample.js
@@ -1,13 +1,14 @@
 import Episode from '../models/episode.model';
-import PathChallenge from '../challenges/path/model';
 import Question from '../models/quiz/question.model';
 import questionData from '../models/quiz/question.data';
+import Challenge from '../models/challenge.model';
 
 export default class EpisodeSampleService {
 	static getTestEpisode(room) {
+		let currentChallenge = new Challenge(room.playersStillPlaying, 'Test', '', 10, 3, [], 'game', [], 'test');
 		return new Episode(
 			room.playersStillPlaying,
-			[ new PathChallenge(room.playersStillPlaying) ],
+			[ currentChallenge ],
 			questionData.map((q) => new Question(q.text, q.type, q.choices))
 		);
 	}
