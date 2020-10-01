604b40a2b56086f329540ef3154572e0b72b3c4c
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-09-30 23:41:24 -0400

    Remove unecessary param

--- src/interfaces/controller.ts
+++ src/interfaces/controller.ts
@@ -1,6 +1,5 @@
 import RoomController from '../controllers/room.controller';
-import ChallengeController from '../controllers/challenge.controller';
 
 export default class Controller {
-	constructor(protected roomController: RoomController, protected challengeController: ChallengeController) {}
+	constructor(protected roomController: RoomController) {}
 }
