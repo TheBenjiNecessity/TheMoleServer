1b39820a194e2b1367d13ed1d6d11f67efd9408c
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-10-01 00:04:50 -0400

    Remove unused properties

--- src/challenges/button/controller.ts
+++ src/challenges/button/controller.ts
@@ -4,9 +4,6 @@ import { CHALLENGE_EVENTS } from '../../contants/challenge.constants';
 import Controller from '../../interfaces/controller';
 
 export default class ButtonChallengeController extends Controller {
-	roomControllerInstance: Function;
-	challengeControllerInstance: Function;
-
 	constructor(protected roomController: RoomController, protected challengeController: ChallengeController) {
 		super(roomController);
 	}
@@ -14,12 +11,12 @@ export default class ButtonChallengeController extends Controller {
 	releasedButton(roomcode, playerName) {
 		let event = 'setPlayerReleasedButton';
 		let message = 'button-player-released';
-		let room = this.roomControllerInstance().performEventOnChallenge(roomcode, event, playerName);
+		let room = this.roomController.performEventOnChallenge(roomcode, event, playerName);
 		let buttonChallenge = room.currentEpisode.currentChallenge;
 
 		// If all buttons are released then the game is over
 		if (buttonChallenge.allButtonsReleased) {
-			this.roomControllerInstance().performEventOnChallenge(roomcode, CHALLENGE_EVENTS.END_CHALLENGE);
+			this.roomController.performEventOnChallenge(roomcode, CHALLENGE_EVENTS.END_CHALLENGE);
 			message = 'challenge-end';
 		}
 
@@ -29,7 +26,7 @@ export default class ButtonChallengeController extends Controller {
 	touchedButton(roomcode, playerName, timerTickCallback, timerDoneCallback, interval = 1000, timerLength = 600000) {
 		let message = 'button-player-pressed';
 		let event = 'setPlayerPressedButton';
-		let room = this.roomControllerInstance().performEventOnChallenge(roomcode, event, playerName);
+		let room = this.roomController.performEventOnChallenge(roomcode, event, playerName);
 		let buttonChallenge = room.currentEpisode.currentChallenge;
 
 		// If it is the start of the game and everyone touches their button then the game begins
@@ -42,13 +39,13 @@ export default class ButtonChallengeController extends Controller {
 	}
 
 	receivedPuzzleAnswer(roomcode, playerName, answer) {
-		let room = this.roomControllerInstance().getRoom(roomcode);
+		let room = this.roomController.getRoom(roomcode);
 		let message = 'button-player-answered';
 		let buttonChallenge = room.currentEpisode.currentChallenge;
 
 		if (buttonChallenge.isPlayerAnswerCorrect(answer)) {
-			this.roomControllerInstance().giveObjectsToPlayer(roomcode, playerName, 'exemption', 1);
-			this.roomControllerInstance().performEventOnChallenge(roomcode, CHALLENGE_EVENTS.END_CHALLENGE);
+			this.roomController.giveObjectsToPlayer(roomcode, playerName, 'exemption', 1);
+			this.roomController.performEventOnChallenge(roomcode, CHALLENGE_EVENTS.END_CHALLENGE);
 			message = 'challenge-end';
 		}
 
