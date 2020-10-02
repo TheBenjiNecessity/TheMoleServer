d75a9eac6a6b121205ae8a3c6c2e3b2a7c14169d
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-10-02 14:00:12 -0400

    Fix roomcontroller tests

--- tests/room.controller.test.ts
+++ tests/room.controller.test.ts
@@ -4,153 +4,189 @@ import Player from '../src/models/player.model';
 import Room from '../src/models/room.model';
 import RoomService from '../src/services/room/room.service';
 import RoomSampleService from './room.sample';
+import WebSocketService from '../src/services/websocket.service';
+import EpisodeSampleService from './episode.sample';
 
 const ROOMCODE_REGEX = /[A-Z]{4}/;
 
-test('Checks "roomCodeAlreadyExists" method', () => {
-	let room = RoomSampleService.getTestRoomWithTenPlayers();
-	RoomController.getInstance().setRoom(room);
+let rooms: { [id: string]: Room } = {};
 
-	expect(RoomController.getInstance().roomCodeAlreadyExists(room.roomcode)).toBe(true);
-	expect(RoomController.getInstance().roomCodeAlreadyExists('NOOP')).toBe(false);
+function getMockRoomController() {
+	let webSocketService = new WebSocketService(null);
+	return new RoomController(
+		webSocketService,
+		[],
+		() => rooms,
+		(r) => {
+			rooms = r;
+		}
+	);
+}
+
+function getMockRoom(numPlayers) {
+	let room = RoomSampleService.getTestRoomForNumPlayers(numPlayers);
+	room.currentEpisode = EpisodeSampleService.getTestEpisode(room);
+	return room;
+}
+
+function getMockComponents(numPlayers) {
+	let room = getMockRoom(numPlayers);
+	let roomController = getMockRoomController();
+
+	roomController.setRoom(room);
+
+	return { room, roomController };
+}
+
+test('Checks "roomCodeAlreadyExists" method', () => {
+	let { room, roomController } = getMockComponents(10);
+
+	expect(roomController.roomCodeAlreadyExists(room.roomcode)).toBe(true);
+	expect(roomController.roomCodeAlreadyExists('NOOP')).toBe(false);
 });
 
 test('Checks "generateRandomRoomCodeNotUsed" method', () => {
-	let room = RoomSampleService.getTestRoomWithTenPlayers(); // Gets room with roomcode 'TEST'
-	RoomController.getInstance().setRoom(room);
+	let { room, roomController } = getMockComponents(10);
 
-	let roomcode = RoomController.getInstance().generateRandomRoomCodeNotUsed();
+	let roomcode = roomController.generateRandomRoomCodeNotUsed();
 
 	expect(ROOMCODE_REGEX.test(roomcode)).toBe(true);
 	expect(RoomService.roomCodeIsABadWord(roomcode)).toBe(false);
 	expect(roomcode !== 'TEST').toBe(true);
-	expect(typeof RoomController.getInstance().getRoom(roomcode)).toBe('undefined');
+	expect(typeof roomController.getRoom(roomcode)).toBe('undefined');
 });
 
 test('Checks "addRoom" method', () => {
-	let roomcode = 'TEST';
-	let room = new Room(roomcode);
-	RoomController.getInstance().setRoom(room);
-	expect(typeof RoomController.getInstance().rooms[roomcode]).toBe('object');
-	expect(RoomController.getInstance().rooms[roomcode].roomcode).toBe(roomcode);
+	let roomController = getMockRoomController();
+
+	expect(Object.keys(roomController.rooms).length).toBe(0);
+
+	roomController.addRoom();
+
+	expect(Object.keys(roomController.rooms).length).toBe(1);
+
+	roomController.deleteRoom(Object.keys(roomController.rooms)[0]);
+
+	expect(Object.keys(roomController.rooms).length).toBe(0);
 });
 
 test('Checks getRoom/setRoom methods', () => {
-	let room = RoomSampleService.getTestRoomWithTenPlayers(); // Gets room with roomcode 'TEST'
+	let { room, roomController } = getMockComponents(10);
 	let { roomcode } = room;
-	RoomController.getInstance().setRoom(room);
 
-	expect(typeof RoomController.getInstance().rooms[roomcode]).toBe('object');
-	expect(RoomController.getInstance().rooms[roomcode].roomcode).toBe(roomcode);
-	expect(typeof RoomController.getInstance().getRoom('TEST')).toBe('object');
-	expect(RoomController.getInstance().getRoom('TEST').roomcode).toBe('TEST');
+	expect(typeof roomController.rooms[roomcode]).toBe('object');
+	expect(roomController.rooms[roomcode].roomcode).toBe(roomcode);
+	expect(typeof roomController.getRoom('TEST')).toBe('object');
+	expect(roomController.getRoom('TEST').roomcode).toBe('TEST');
 });
 
 test('Checks "addPlayerToRoom" method', () => {
-	let room = RoomSampleService.getTestRoomWithNoPlayers(); // Gets room with roomcode 'TEST'
-	RoomController.getInstance().setRoom(room);
+	let { room, roomController } = getMockComponents(0);
 
-	RoomController.getInstance().addPlayerToRoom('TEST', new Player('test11'));
+	// Init test
+	expect(roomController.getRoom('TEST').players.length).toBe(0);
 
-	expect(RoomController.getInstance().getRoom('TEST').players.length).toBe(1);
-	expect(RoomController.getInstance().getRoom('TEST').players[0].name).toBe('test11');
+	// Test adding first player
+	roomController.addPlayerToRoom('TEST', new Player('test11'));
 
-	RoomController.getInstance().addPlayerToRoom('TEST', new Player('test11'));
+	expect(roomController.getRoom('TEST').players.length).toBe(1);
+	expect(roomController.getRoom('TEST').players[0].name).toBe('test11');
 
-	expect(RoomController.getInstance().getRoom('TEST').players.length).toBe(1);
-	expect(RoomController.getInstance().getRoom('TEST').players[0].name).toBe('test11');
+	// Test trying to add the same player twice
+	roomController.addPlayerToRoom('TEST', new Player('test11'));
 
-	RoomController.getInstance().addPlayerToRoom('TEST', new Player('test12'));
+	expect(roomController.getRoom('TEST').players.length).toBe(1);
+	expect(roomController.getRoom('TEST').players[0].name).toBe('test11');
 
-	expect(RoomController.getInstance().getRoom('TEST').players.length).toBe(2);
-	expect(RoomController.getInstance().getRoom('TEST').players[1].name).toBe('test12');
+	// Test adding second player
+	roomController.addPlayerToRoom('TEST', new Player('test12'));
+
+	expect(roomController.getRoom('TEST').players.length).toBe(2);
+	expect(roomController.getRoom('TEST').players[1].name).toBe('test12');
 });
 
 test('Checks "removePlayerToRoom" method', () => {}); //TODO
 
 test('Checks "giveObjectsToPlayer/removeObjectsFromPlayer" methods', () => {
-	let room = RoomSampleService.getTestRoomWithTenPlayers(); // Gets room with roomcode 'TEST'
+	let { room, roomController } = getMockComponents(10);
 	let { roomcode } = room;
 	let exemption = 'exemption';
 	let joker = 'joker';
 	let blackExemption = 'black-exemption';
-	RoomController.getInstance().setRoom(room);
 
 	let player = room.players[0];
 
-	RoomController.getInstance().giveObjectsToPlayer(roomcode, player.name, exemption, 2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[exemption]).toBe(2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[joker]).toBe(0);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[blackExemption]).toBe(0);
+	roomController.giveObjectsToPlayer(roomcode, player.name, exemption, 2);
+	expect(roomController.getRoom(roomcode).players[0].objects[exemption]).toBe(2);
+	expect(roomController.getRoom(roomcode).players[0].objects[joker]).toBe(0);
+	expect(roomController.getRoom(roomcode).players[0].objects[blackExemption]).toBe(0);
 
-	RoomController.getInstance().giveObjectsToPlayer(roomcode, player.name, joker, 2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[exemption]).toBe(2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[joker]).toBe(2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[blackExemption]).toBe(0);
+	roomController.giveObjectsToPlayer(roomcode, player.name, joker, 2);
+	expect(roomController.getRoom(roomcode).players[0].objects[exemption]).toBe(2);
+	expect(roomController.getRoom(roomcode).players[0].objects[joker]).toBe(2);
+	expect(roomController.getRoom(roomcode).players[0].objects[blackExemption]).toBe(0);
 
-	RoomController.getInstance().giveObjectsToPlayer(roomcode, player.name, blackExemption, 2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[exemption]).toBe(2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[joker]).toBe(2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[blackExemption]).toBe(2);
+	roomController.giveObjectsToPlayer(roomcode, player.name, blackExemption, 2);
+	expect(roomController.getRoom(roomcode).players[0].objects[exemption]).toBe(2);
+	expect(roomController.getRoom(roomcode).players[0].objects[joker]).toBe(2);
+	expect(roomController.getRoom(roomcode).players[0].objects[blackExemption]).toBe(2);
 
-	RoomController.getInstance().removeObjectsFromPlayer(roomcode, player.name, exemption, 1);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[exemption]).toBe(1);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[joker]).toBe(2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[blackExemption]).toBe(2);
+	roomController.removeObjectsFromPlayer(roomcode, player.name, exemption, 1);
+	expect(roomController.getRoom(roomcode).players[0].objects[exemption]).toBe(1);
+	expect(roomController.getRoom(roomcode).players[0].objects[joker]).toBe(2);
+	expect(roomController.getRoom(roomcode).players[0].objects[blackExemption]).toBe(2);
 
-	RoomController.getInstance().removeObjectsFromPlayer(roomcode, player.name, joker, 2);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[exemption]).toBe(1);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[joker]).toBe(0);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[blackExemption]).toBe(2);
+	roomController.removeObjectsFromPlayer(roomcode, player.name, joker, 2);
+	expect(roomController.getRoom(roomcode).players[0].objects[exemption]).toBe(1);
+	expect(roomController.getRoom(roomcode).players[0].objects[joker]).toBe(0);
+	expect(roomController.getRoom(roomcode).players[0].objects[blackExemption]).toBe(2);
 
-	RoomController.getInstance().removeObjectsFromPlayer(roomcode, player.name, blackExemption, 3);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[exemption]).toBe(1);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[joker]).toBe(0);
-	expect(RoomController.getInstance().getRoom(roomcode).players[0].objects[blackExemption]).toBe(0);
+	roomController.removeObjectsFromPlayer(roomcode, player.name, blackExemption, 3);
+	expect(roomController.getRoom(roomcode).players[0].objects[exemption]).toBe(1);
+	expect(roomController.getRoom(roomcode).players[0].objects[joker]).toBe(0);
+	expect(roomController.getRoom(roomcode).players[0].objects[blackExemption]).toBe(0);
 });
 
 test('Checks "addPoints/removePoints" methods', () => {
-	let room = RoomSampleService.getTestRoomWithTenPlayers(); // Gets room with roomcode 'TEST'
+	let { room, roomController } = getMockComponents(10);
 	let { roomcode } = room;
-	RoomController.getInstance().setRoom(room);
 
-	expect(RoomController.getInstance().getRoom(roomcode).points).toBe(0);
+	expect(roomController.getRoom(roomcode).points).toBe(0);
 
-	RoomController.getInstance().addPoints(roomcode);
-	expect(RoomController.getInstance().getRoom(roomcode).points).toBe(1);
+	roomController.addPoints(roomcode);
+	expect(roomController.getRoom(roomcode).points).toBe(1);
 
-	RoomController.getInstance().addPoints(roomcode);
-	expect(RoomController.getInstance().getRoom(roomcode).points).toBe(2);
+	roomController.addPoints(roomcode);
+	expect(roomController.getRoom(roomcode).points).toBe(2);
 
-	RoomController.getInstance().addPoints(roomcode, 1);
-	expect(RoomController.getInstance().getRoom(roomcode).points).toBe(3);
+	roomController.addPoints(roomcode, 1);
+	expect(roomController.getRoom(roomcode).points).toBe(3);
 
-	RoomController.getInstance().addPoints(roomcode, 3);
-	expect(RoomController.getInstance().getRoom(roomcode).points).toBe(6);
+	roomController.addPoints(roomcode, 3);
+	expect(roomController.getRoom(roomcode).points).toBe(6);
 
-	RoomController.getInstance().removePoints(roomcode);
-	expect(RoomController.getInstance().getRoom(roomcode).points).toBe(5);
+	roomController.removePoints(roomcode);
+	expect(roomController.getRoom(roomcode).points).toBe(5);
 
-	RoomController.getInstance().removePoints(roomcode, 1);
-	expect(RoomController.getInstance().getRoom(roomcode).points).toBe(4);
+	roomController.removePoints(roomcode, 1);
+	expect(roomController.getRoom(roomcode).points).toBe(4);
 
-	RoomController.getInstance().removePoints(roomcode, 2);
-	expect(RoomController.getInstance().getRoom(roomcode).points).toBe(2);
+	roomController.removePoints(roomcode, 2);
+	expect(roomController.getRoom(roomcode).points).toBe(2);
 
-	RoomController.getInstance().removePoints(roomcode, 3);
-	expect(RoomController.getInstance().getRoom(roomcode).points).toBe(0);
+	roomController.removePoints(roomcode, 3);
+	expect(roomController.getRoom(roomcode).points).toBe(0);
 });
 
 test('Checks "moveNext" method', () => {
-	let room = RoomSampleService.getTestRoomWithTenPlayers(); // Gets room with roomcode 'TEST'
+	let { room, roomController } = getMockComponents(10);
 	let { roomcode } = room;
-	RoomController.getInstance().setRoom(room);
 
-	expect(RoomController.getInstance().getRoom(roomcode).state).toBe(ROOM_STATE.LOBBY);
+	expect(roomController.getRoom(roomcode).state).toBe(ROOM_STATE.LOBBY);
 
-	RoomController.getInstance().moveNext({ roomcode });
+	roomController.moveNext({ roomcode });
 
-	expect(RoomController.getInstance().getRoom(roomcode).state).toBe(ROOM_STATE.WELCOME);
+	expect(roomController.getRoom(roomcode).state).toBe(ROOM_STATE.WELCOME);
 });
 
 test('Checks "quizDone" method', () => {}); //TODO
