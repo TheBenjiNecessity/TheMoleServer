ac0e7f5ce92913f6fff092cc2122a8df6de876b3
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-10-02 15:23:10 -0400

    Move methods to controller

--- src/controllers/challenge.controller.ts
+++ src/controllers/challenge.controller.ts
@@ -1,5 +1,5 @@
 import RoomController from './room.controller';
-import { CHALLENGE_EVENTS } from '../contants/challenge.constants';
+import { CHALLENGE_EVENTS, CHALLENGE_STATES, CHALLENGE_SOCKET_EVENTS } from '../contants/challenge.constants';
 import Controller from '../interfaces/controller';
 
 const MILLISECONDS_IN_SECOND = 1000;
@@ -7,6 +7,47 @@ const MILLISECONDS_IN_SECOND = 1000;
 export default class ChallengeController extends Controller {
 	constructor(protected roomController: RoomController) {
 		super(roomController);
+	}
+
+	raiseHand(roomcode, player, role): string {
+		let room = this.roomController.getRoom(roomcode);
+		if (room.currentEpisode.currentChallenge.state !== CHALLENGE_STATES.ROLE_SELECTION) {
+			return null;
+		}
+
+		this.roomController.performEventOnChallenge(roomcode, CHALLENGE_EVENTS.RAISE_HAND_FOR_PLAYER, player, role);
+
+		return CHALLENGE_SOCKET_EVENTS.RAISE_HAND;
+	}
+
+	agreeToRoles(roomcode, player): string {
+		let room = this.roomController.getRoom(roomcode);
+		let { currentChallenge } = room.currentEpisode;
+		if (currentChallenge.state !== CHALLENGE_STATES.ROLE_SELECTION || !currentChallenge.raisedHandsAreValid) {
+			return;
+		}
+
+		this.roomController.performEventOnChallenge(roomcode, CHALLENGE_EVENTS.ADD_AGREED_PLAYER, player);
+
+		if (room.currentEpisode.currentChallenge.hasMajorityVoteForAgreedPlayers) {
+			room.currentEpisode.currentChallenge.moveNext();
+			this.roomController.setRoom(room);
+			return CHALLENGE_SOCKET_EVENTS.MOVE_NEXT;
+		} else {
+			return CHALLENGE_SOCKET_EVENTS.AGREE_TO_ROLES;
+		}
+	}
+
+	addPlayerVote(roomcode, player): string {
+		let event = CHALLENGE_EVENTS.SET_VOTED_PLAYER;
+		this.roomController.performEventOnChallenge(roomcode, event, player);
+		return CHALLENGE_SOCKET_EVENTS.VOTED_PLAYER;
+	}
+
+	removePlayerVote(roomcode, player): string {
+		let event = CHALLENGE_EVENTS.REMOVE_VOTED_PLAYER;
+		this.roomController.performEventOnChallenge(roomcode, event, player);
+		return CHALLENGE_SOCKET_EVENTS.REMOVE_VOTED_PLAYER;
 	}
 
 	startTimer(
--- src/controllers/challenge.socket-handler.ts
+++ src/controllers/challenge.socket-handler.ts
@@ -1,6 +1,5 @@
 import RoomController from './room.controller';
 import WebSocketService from '../services/websocket.service';
-import { CHALLENGE_EVENTS, CHALLENGE_STATES, CHALLENGE_SOCKET_EVENTS } from '../contants/challenge.constants';
 import SocketHandler from '../interfaces/socket-handler';
 import ChallengeController from './challenge.controller';
 
@@ -22,44 +21,23 @@ export default class ChallengeSocketHandler extends SocketHandler {
 	}
 
 	raiseHand({ roomcode, player, role }) {
-		let room = this.roomController.getRoom(roomcode);
-		if (room.currentEpisode.currentChallenge.state !== CHALLENGE_STATES.ROLE_SELECTION) {
-			return;
-		}
-
-		this.roomController.performEventOnChallenge(roomcode, CHALLENGE_EVENTS.RAISE_HAND_FOR_PLAYER, player, role);
-
-		return this.webSocketService.sendToRoom(roomcode, CHALLENGE_SOCKET_EVENTS.RAISE_HAND);
+		let message = this.challengeController.raiseHand(roomcode, player, role);
+		return this.webSocketService.sendToRoom(roomcode, message);
 	}
 
 	agreeToRoles({ roomcode, player }) {
-		let room = this.roomController.getRoom(roomcode);
-		let { currentChallenge } = room.currentEpisode;
-		if (currentChallenge.state !== CHALLENGE_STATES.ROLE_SELECTION || !currentChallenge.raisedHandsAreValid) {
-			return;
-		}
-
-		this.roomController.performEventOnChallenge(roomcode, CHALLENGE_EVENTS.ADD_AGREED_PLAYER, player);
-
-		if (room.currentEpisode.currentChallenge.hasMajorityVoteForAgreedPlayers) {
-			room.currentEpisode.currentChallenge.moveNext();
-			this.roomController.setRoom(room);
-			return this.webSocketService.sendToRoom(roomcode, CHALLENGE_SOCKET_EVENTS.MOVE_NEXT);
-		} else {
-			return this.webSocketService.sendToRoom(roomcode, CHALLENGE_SOCKET_EVENTS.AGREE_TO_ROLES);
-		}
+		let message = this.challengeController.agreeToRoles(roomcode, player);
+		return this.webSocketService.sendToRoom(roomcode, message);
 	}
 
 	addPlayerVote({ roomcode, player }) {
-		let event = CHALLENGE_EVENTS.SET_VOTED_PLAYER;
-		this.roomController.performEventOnChallenge(roomcode, event, player);
-		return this.webSocketService.sendToRoom(roomcode, CHALLENGE_SOCKET_EVENTS.VOTED_PLAYER);
+		let message = this.challengeController.addPlayerVote(roomcode, player);
+		return this.webSocketService.sendToRoom(roomcode, message);
 	}
 
 	removePlayerVote({ roomcode, player }) {
-		let event = CHALLENGE_EVENTS.REMOVE_VOTED_PLAYER;
-		this.roomController.performEventOnChallenge(roomcode, event, player);
-		return this.webSocketService.sendToRoom(roomcode, CHALLENGE_SOCKET_EVENTS.REMOVE_VOTED_PLAYER);
+		let message = this.challengeController.removePlayerVote(roomcode, player);
+		return this.webSocketService.sendToRoom(roomcode, message);
 	}
 
 	startTimer(
