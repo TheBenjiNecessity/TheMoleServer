f9c2ec501b9f57426c76f6053babeae36c6f1027
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-10-19 22:18:17 -0400

    Use updated model

--- src/challenges/platter/controller.ts
+++ src/challenges/platter/controller.ts
@@ -2,22 +2,35 @@ import RoomControllerCreator from '../../controllers/room.controller';
 import Controller from '../../interfaces/controller';
 import RoomController from '../../controllers/room.controller';
 import ChallengeController from '../../controllers/challenge.controller';
+import { CHALLENGE_EVENTS } from '../../contants/challenge.constants';
+import PlatterChallenge from './model';
+
+const POINTS = 3;
 
 export default class PlatterChallengeController extends Controller {
 	constructor(protected roomController: RoomController, protected challengeController: ChallengeController) {
 		super(roomController);
 	}
 
-	chooseExemption(roomcode, player) {
-		let event = 'take-exemption';
-		this.roomController.performEventOnChallenge(roomcode, event);
-		this.roomController.giveObjectsToPlayer(roomcode, player, 'exemption', 1);
+	chooseExemption(roomcode: string, playerName: string) {
+		this.roomController.performEventOnChallenge(roomcode, 'takeExemption', playerName);
+		this.roomController.giveObjectsToPlayer(roomcode, playerName, 'exemption', 1);
+		this.roomController.performEventOnChallenge(roomcode, CHALLENGE_EVENTS.END_CHALLENGE);
 		return 'took-exemption';
 	}
 
-	chooseMoney(roomcode) {
-		let event = 'take-money';
-		this.roomController.performEventOnChallenge(roomcode, event);
+	chooseMoney(roomcode: string, playerName: string) {
+		let room = this.roomController.getRoom(roomcode);
+		let platterChallenge = room.currentEpisode.currentChallenge as PlatterChallenge;
+		if (!platterChallenge.playerTookMoney(playerName)) {
+			room = this.roomController.performEventOnChallenge(roomcode, 'takeMoney', playerName);
+			platterChallenge = room.currentEpisode.currentChallenge as PlatterChallenge;
+			this.roomController.addPoints(roomcode, POINTS);
+			if (platterChallenge.allMoneyWasTaken) {
+				this.roomController.performEventOnChallenge(roomcode, CHALLENGE_EVENTS.END_CHALLENGE);
+			}
+		}
+
 		return 'took-money';
 	}
 }
