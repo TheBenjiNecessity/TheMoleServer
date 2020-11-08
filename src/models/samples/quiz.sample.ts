809bd69cd1ae1b7cc158486b299068b7b0a6011e
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-11-08 15:47:31 -0500

    Fix quiz sample service bugs

--- src/models/samples/quiz.sample.ts
+++ src/models/samples/quiz.sample.ts
@@ -1,4 +1,3 @@
-import questionData from '../quiz/question.data';
 import Question from '../quiz/question.model';
 import Quiz from '../quiz/quiz.model';
 import QuizAnswers from '../quiz/quiz-answers.model';
@@ -6,72 +5,80 @@ import Answer from '../quiz/quiz-answer.model';
 
 export default class QuizSampleService {
 	static getTestQuiz(): Quiz {
-        let questions: Question[] = questionData;
+		let question = {
+			text: 'Question text?',
+			type: 'choices',
+			choices: [ 'Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5' ]
+		};
+		let questions: Question[] = [];
+
+		for (let i = 0; i < 20; i++) {
+			questions.push(question);
+		}
+
 		return { questions };
 	}
 
 	static getQuizAnswers(quiz: Quiz, indices: number[]): QuizAnswers {
-        let answers: Answer[] = quiz.questions.map((q, i) => new Answer(q, indices[i]));
-        return new QuizAnswers(answers, 1);
-    }
+		let answers: Answer[] = quiz.questions.map((q, i) => new Answer(q, indices[i]));
+		return new QuizAnswers(answers, 1);
+	}
 
-    static getMoleQuizAnswers(quiz: Quiz): QuizAnswers {
-        return QuizSampleService.getQuizAnswers(quiz, quiz.questions.map(q => 0));
-    }
-    
-    // Three quarter answers are correct
-    static getGreatQuizAnswers(moleQuizAnswers: QuizAnswers, time: number): QuizAnswers {
-        let indices: number[] = []
-        for (let i = 0; i < moleQuizAnswers.answers.length; i++) {
-            let answer = moleQuizAnswers.answers[i];
-            if (i <= moleQuizAnswers.answers.length / 4) {
-                indices.push((answer.answerIndex + 1) % answer.question.choices.length);
-            } else {
-                indices.push(answer.answerIndex);
-            }
-        }
-        return new QuizAnswers(indices.map((i, index) => new Answer(moleQuizAnswers.answers[index].question, i)), time);
-    }
+	static getMoleQuizAnswers(quiz: Quiz): QuizAnswers {
+		return QuizSampleService.getQuizAnswers(quiz, quiz.questions.map((q) => 0));
+	}
 
-    // Half of all answers are correct
-    static getGoodQuizAnswers(moleQuizAnswers: QuizAnswers, time: number) {
-        let indices: number[] = []
-        for (let i = 0; i < moleQuizAnswers.answers.length; i++) {
-            let answer = moleQuizAnswers.answers[i];
-            if (i % 2 === 0) {
-                indices.push((answer.answerIndex + 1) % answer.question.choices.length);
-            } else {
-                indices.push(answer.answerIndex);
-            }
-        }
-        return new QuizAnswers(indices.map((i, index) => new Answer(moleQuizAnswers.answers[index].question, i)), time);
-    }
+	// Three quarter answers are correct
+	static getGreatQuizAnswers(moleQuizAnswers: QuizAnswers, time: number): QuizAnswers {
+		let indices: number[] = [];
 
-    // Three quarter answers are wrong
-    static getBadQuizAnswers(moleQuizAnswers: QuizAnswers, time: number) {
-        let indices: number[] = []
-        for (let i = 0; i < moleQuizAnswers.answers.length; i++) {
-            let answer = moleQuizAnswers.answers[i];
-            if (i >= moleQuizAnswers.answers.length / 4) {
-                indices.push((answer.answerIndex + 1) % answer.question.choices.length);
-            } else {
-                indices.push(answer.answerIndex);
-            }
-        }
-        return new QuizAnswers(indices.map((i, index) => new Answer(moleQuizAnswers.answers[index].question, i)), time);
-    }
+		for (let i = 0; i < moleQuizAnswers.answers.length; i++) {
+			let answer = moleQuizAnswers.answers[i];
+			if (i >= 0 && i <= 4) {
+				indices.push((answer.answerIndex + 1) % answer.question.choices.length);
+			} else {
+				indices.push(answer.answerIndex);
+			}
+		}
 
-    // All answers are wrong
-    static getLoserQuizAnswers(moleQuizAnswers: QuizAnswers, time: number) {
-        let indices: number[] = []
-        for (let i = 0; i < moleQuizAnswers.answers.length; i++) {
-            let answer = moleQuizAnswers.answers[i];
-            if (i >= moleQuizAnswers.answers.length / 4) {
-                indices.push((answer.answerIndex + 1) % answer.question.choices.length);
-            } else {
-                indices.push(answer.answerIndex);
-            }
-        }
-        return new QuizAnswers(indices.map((i, index) => new Answer(moleQuizAnswers.answers[index].question, i)), time);
-    }
+		return new QuizAnswers(indices.map((i, index) => new Answer(moleQuizAnswers.answers[index].question, i)), time);
+	}
+
+	// Half of all answers are correct
+	static getGoodQuizAnswers(moleQuizAnswers: QuizAnswers, time: number) {
+		let indices: number[] = [];
+		for (let i = 0; i < moleQuizAnswers.answers.length; i++) {
+			let answer = moleQuizAnswers.answers[i];
+			if (i < 10) {
+				indices.push((answer.answerIndex + 1) % answer.question.choices.length);
+			} else {
+				indices.push(answer.answerIndex);
+			}
+		}
+		return new QuizAnswers(indices.map((i, index) => new Answer(moleQuizAnswers.answers[index].question, i)), time);
+	}
+
+	// Three quarter answers are wrong
+	static getBadQuizAnswers(moleQuizAnswers: QuizAnswers, time: number) {
+		let indices: number[] = [];
+		for (let i = 0; i < moleQuizAnswers.answers.length; i++) {
+			let answer = moleQuizAnswers.answers[i];
+			if (i >= 5) {
+				indices.push((answer.answerIndex + 1) % answer.question.choices.length);
+			} else {
+				indices.push(answer.answerIndex);
+			}
+		}
+		return new QuizAnswers(indices.map((i, index) => new Answer(moleQuizAnswers.answers[index].question, i)), time);
+	}
+
+	// All answers are wrong
+	static getLoserQuizAnswers(moleQuizAnswers: QuizAnswers, time: number) {
+		let indices: number[] = [];
+		for (let i = 0; i < moleQuizAnswers.answers.length; i++) {
+			let answer = moleQuizAnswers.answers[i];
+			indices.push((answer.answerIndex + 1) % answer.question.choices.length);
+		}
+		return new QuizAnswers(indices.map((i, index) => new Answer(moleQuizAnswers.answers[index].question, i)), time);
+	}
 }
