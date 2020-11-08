f80150f1ad3261c365731d2d664886c45474a0ad
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-11-08 15:50:26 -0500

    Add types

--- src/models/quiz/quiz-answer.model.ts
+++ src/models/quiz/quiz-answer.model.ts
@@ -1,9 +1,9 @@
 import Question from './question.model';
 
 export default class Answer {
-	constructor(public question: Question, public answerIndex: number) { }
+	constructor(public question: Question, public answerIndex: number) {}
 
-	isCorrect(moleAnswer) {
-		return this.question.text === moleAnswer.question.text && this.answerIndex === moleAnswer.answer;
+	isCorrect(moleAnswer: Answer): boolean {
+		return this.question.text === moleAnswer.question.text && this.answerIndex === moleAnswer.answerIndex;
 	}
 }
