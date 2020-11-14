b41907d5af0f986f8db472a711b14897c223a770
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-11-14 15:03:03 -0500

    Add sample role challenge

--- src/models/samples/challenge.sample.ts
+++ src/models/samples/challenge.sample.ts
@@ -1,14 +1,34 @@
+import ChallengeController from '../../controllers/challenge.controller';
+import RoomController from '../../controllers/room.controller';
 import Challenge from '../challenge.model';
 import Player from '../player.model';
 
 class SampleChallenge extends Challenge {
 	constructor(players: Player[]) {
-		super(players, 'Test title', 'Test description', [], 'game', [], 'Test type');
+		super(players, 'Test title', 'Test description', [], Challenge.CHALLENGE_STATES.IN_GAME, [], 'Test type');
+	}
+}
+
+class SampleRoleChallenge extends Challenge {
+	constructor(players: Player[]) {
+		super(
+			players,
+			'Test title',
+			'Test description',
+			[],
+			Challenge.CHALLENGE_STATES.ROLE_SELECTION,
+			[],
+			'Test type'
+		);
 	}
 }
 
 export default class ChallengeSampleService {
 	static getTestChallenge(room) {
 		return new SampleChallenge(room.playersStillPlaying);
+	}
+
+	static getTestChallengeWithRoles(room) {
+		return new SampleRoleChallenge(room.playersStillPlaying);
 	}
 }
