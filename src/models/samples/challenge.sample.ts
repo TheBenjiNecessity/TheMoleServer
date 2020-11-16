291db6803e8f1c82c99cc119e63103bb64241838
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-11-15 22:40:25 -0500

    Add new sample data

--- src/models/samples/challenge.sample.ts
+++ src/models/samples/challenge.sample.ts
@@ -1,5 +1,8 @@
 import ChallengeController from '../../controllers/challenge.controller';
 import RoomController from '../../controllers/room.controller';
+import ChallengeData from '../../interfaces/challenge-data';
+import SocketHandler from '../../interfaces/socket-handler';
+import WebSocketService from '../../services/websocket.service';
 import Challenge from '../challenge.model';
 import Player from '../player.model';
 
@@ -17,9 +20,56 @@ class SampleRoleChallenge extends Challenge {
 			'Test description',
 			[],
 			Challenge.CHALLENGE_STATES.ROLE_SELECTION,
-			[],
+			[
+				{ name: 'test 1', numPlayers: Math.floor(players.length / 2) },
+				{ name: 'test 2', numPlayers: Math.ceil(players.length / 2) }
+			],
 			'Test type'
 		);
+	}
+}
+
+class SampleSocketHandler extends SocketHandler {
+	constructor(roomController: RoomController, webSocketService: WebSocketService, socket: any) {
+		super(roomController, webSocketService, socket);
+	}
+}
+
+class SampleChallengeData extends ChallengeData {
+	constructor() {
+		super({
+			en: {
+				title: 'Test',
+				description: '',
+				questions: [
+					{
+						text: 'Test?',
+						type: 'choices',
+						choices: [ 'Yes', 'No' ]
+					}
+				]
+			}
+		});
+	}
+
+	get type(): string {
+		return 'test';
+	}
+
+	get maxPlayers(): number {
+		return 10;
+	}
+
+	get minPlayers(): number {
+		return 3;
+	}
+
+	setupSocketHandler(roomController: RoomController, webSocketService: WebSocketService, socket: any): SocketHandler {
+		return new SampleSocketHandler(roomController, webSocketService, socket);
+	}
+
+	getModel(players, lang): Challenge {
+		return new SampleRoleChallenge(players);
 	}
 }
 
@@ -30,5 +80,9 @@ export default class ChallengeSampleService {
 
 	static getTestChallengeWithRoles(room) {
 		return new SampleRoleChallenge(room.playersStillPlaying);
+	}
+
+	static getTestChallengeData() {
+		return new SampleChallengeData();
 	}
 }
--- src/models/samples/room-controller.sample.ts
+++ src/models/samples/room-controller.sample.ts
@@ -1,14 +1,15 @@
 import RoomController from '../../controllers/room.controller';
+import ChallengeData from '../../interfaces/challenge-data';
 import WebSocketService from '../../services/websocket.service';
 import Room from '../room.model';
 
 let rooms: { [id: string]: Room } = {};
 
-export function getMockRoomController() {
+export function getMockRoomController(challengeData: ChallengeData[] = []) {
 	rooms = {};
 	return new RoomController(
 		new WebSocketService(null),
-		[],
+		challengeData,
 		() => rooms,
 		(r) => {
 			rooms = r;
