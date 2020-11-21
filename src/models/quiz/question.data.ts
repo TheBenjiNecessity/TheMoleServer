1a8f782acaa4b7330706dadb6fa2a63116ba0307
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-11-20 22:33:57 -0500

    Add localizations to questions

--- src/models/quiz/question.data.ts
+++ src/models/quiz/question.data.ts
@@ -1,50 +1,52 @@
-export default [
-	{
-		text: 'What is the eye color of the mole?',
-		type: 'choices',
-		choices: [ 'Blue', 'Green', 'Hazel', 'Gray', 'Multi-colored', 'Brown', 'Amber', 'Other' ]
-	},
-	{
-		text: 'How tall is the mole?',
-		type: 'choices',
-		choices: [ "Less than 5'", "Between 5' and 5'8\"", 'Between 5\'9" and 6\'3"', '6\'4" or above' ]
-	},
-	{
-		text: 'What month was the mole born in?',
-		type: 'choices',
-		choices: [
-			'January',
-			'February',
-			'March',
-			'April',
-			'May',
-			'June',
-			'July',
-			'August',
-			'September',
-			'October',
-			'November',
-			'December'
-		]
-	},
-	{
-		text: 'What decade was the mole born in?',
-		type: 'choices',
-		choices: [ '1940s', '1950s', '1960s', '1970s', '1980s', '1990s', '2000s', '2010s' ]
-	},
-	{
-		text: "What is the mole's hair color?",
-		type: 'choices',
-		choices: [ 'Blonde', 'Brown', 'Black', 'Red', 'Other (dyed)' ]
-	},
-	{
-		text: 'Is the mole male or female?',
-		type: 'choices',
-		choices: [ 'Male', 'Female' ]
-	},
-	{
-		text: 'From what continent was the mole born?',
-		type: 'choices',
-		choices: [ 'North America', 'South America', 'Asia', 'Africa', 'Australia', 'Europe' ]
-	}
-];
+export default {
+	en: [
+		{
+			text: 'What is the eye color of the mole?',
+			type: 'choices',
+			choices: [ 'Blue', 'Green', 'Hazel', 'Gray', 'Multi-colored', 'Brown', 'Amber', 'Other' ]
+		},
+		{
+			text: 'How tall is the mole?',
+			type: 'choices',
+			choices: [ "Less than 5'", "Between 5' and 5'8\"", 'Between 5\'9" and 6\'3"', '6\'4" or above' ]
+		},
+		{
+			text: 'What month was the mole born in?',
+			type: 'choices',
+			choices: [
+				'January',
+				'February',
+				'March',
+				'April',
+				'May',
+				'June',
+				'July',
+				'August',
+				'September',
+				'October',
+				'November',
+				'December'
+			]
+		},
+		{
+			text: 'What decade was the mole born in?',
+			type: 'choices',
+			choices: [ '1940s', '1950s', '1960s', '1970s', '1980s', '1990s', '2000s', '2010s' ]
+		},
+		{
+			text: "What is the mole's hair color?",
+			type: 'choices',
+			choices: [ 'Blonde', 'Brown', 'Black', 'Red', 'Other (dyed)' ]
+		},
+		{
+			text: 'Is the mole male or female?',
+			type: 'choices',
+			choices: [ 'Male', 'Female' ]
+		},
+		{
+			text: 'From what continent was the mole born?',
+			type: 'choices',
+			choices: [ 'North America', 'South America', 'Asia', 'Africa', 'Australia', 'Europe' ]
+		}
+	]
+};
