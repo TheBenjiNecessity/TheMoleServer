105a4f1f7cb427ee69b13ab6588f205c791a8d2f
TheBenjiNecessity <benjamin.wishart@gmail.com>
2020-12-31 12:28:29 -0500

    Add "getRiddleList" method

--- src/services/game/riddle.service.ts
+++ src/services/game/riddle.service.ts
@@ -1,10 +1,22 @@
 import Riddle from '../../models/riddle.model';
 import riddleData from '../../data/riddle.data';
+import * as _ from 'lodash';
 
-export default class RiddleService {
+export interface IRiddleService {
+	getRandomRiddle(lang: string, type: string): Riddle;
+	getRiddleList(lang: string, type: string, count: number): Riddle[];
+}
+
+export default class RiddleService implements IRiddleService {
 	constructor() {}
 
-	static getRandomRiddle(lang: string, type: string = null): Riddle {
-		return riddleData[lang].filter((r) => (type ? r.type === type : true));
+	getRandomRiddle(lang: string, type: string): Riddle {
+		return this.getRiddleList(lang, type, 1)[0];
+	}
+
+	getRiddleList(lang: string, type: string, count: number): Riddle[] {
+		let riddlesForType = riddleData[lang].filter((r) => (type ? r.type === type : true));
+		let shuffledRiddlesForType = _.shuffle(riddlesForType);
+		return shuffledRiddlesForType.slice(0, count).map((s) => new Riddle(s.type, s.answer, s.type));
 	}
 }
