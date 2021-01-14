36b488bf6f704d79ccfdfadcb5e1a1140575f536
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-01-14 13:45:45 -0500

    Add default param

--- src/services/websocket.service.ts
+++ src/services/websocket.service.ts
@@ -1,7 +1,9 @@
+import Room from '../models/room.model';
+
 export default class WebSocketService {
 	constructor(private socketObj: any) {}
 
-	sendToRoom(room, action) {
+	sendToRoom(room: Room, action: string = 'room-event') {
 		if (this.socketObj) {
 			this.socketObj.in(room.roomcode).emit(action, room);
 		}
